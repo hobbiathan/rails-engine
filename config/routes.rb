6c930715520de1a03efa1c53efdc7482c120b969
Ruben Pasillas <hobbiathan@gmail.com>
2022-03-30 17:40:13 -0600

    add merchant/item route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show] do 
-
+        resources :items, only: [:index] do
+          
+        end 
       end
     end 
   end 
