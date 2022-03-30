bd17f111a75ef3d91023eaf7a99c1855cc7dd844
Ruben Pasillas <hobbiathan@gmail.com>
2022-03-30 14:43:08 -0600

    create merchant_spec request

--- spec/requests/merchants_spec.rb
+++ spec/requests/merchants_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Merchants", type: :request do
+  let!(:merchants) { create_list(:merchant, 5) }
+  let(:merchant_id) { :merchants.first.id }
+
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    before { get '/api/v1/merchants' }
+
+    it 'returns merchants' do
+      expect(json).to_not be_empty
+      expect(json.size).to eq(5)
+    end 
   end
 end
