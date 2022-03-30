6675d5228ed980396cf6112e4c32ed8d9de6f15a
Ruben Pasillas <hobbiathan@gmail.com>
2022-03-30 17:06:02 -0600

    create specific merchant request spec tests

--- spec/requests/merchants_spec.rb
+++ spec/requests/merchants_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Merchants", type: :request do
   let!(:merchants) { create_list(:merchant, 5) }
   let(:merchant_id) { :merchants.first.id }
 
-  describe "GET /index" do
+  describe "GET /api/v1/merchants" do
     before { get '/api/v1/merchants' }
 
     it 'returns merchants' do
@@ -16,4 +16,32 @@ RSpec.describe "Merchants", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'GET /api/v1/merchants/merchant_id' do
+    before { get "/api/v1/#{merchant_id}" }
+
+    context 'when the record exists' do
+      it 'returns the merchant' do
+        expect(json["data"]).not_to be_empty
+        expect(json["data"]["id"]).to eq(merchant_id)
+      end 
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end 
+    end 
+
+    context 'when the record does not exist' do
+      let(:merchant_id) { 12395412341234 } # Yea
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end 
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Merchant/)
+      end 
+    end 
+  end 
+  
 end
