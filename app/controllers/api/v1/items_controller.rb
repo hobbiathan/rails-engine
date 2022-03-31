876b5fb44ed02d6bbb5ef4aa934f705545729e24
Ruben Pasillas <hobbiathan@gmail.com>
2022-03-31 12:07:40 -0600

    finish items#create

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -6,4 +6,15 @@ class Api::V1::ItemsController < ApplicationController
     def show
         render json: ItemSerializer.new(Item.find(params[:id]))
     end 
+
+    def create
+        merchant = Merchant.find(params["merchant_id"])
+        render json: merchant.items.create!(item_params), status: :created
+    end
+
+    private
+
+    def item_params
+        params.permit(:name, :description, :unit_price)
+    end 
 end
