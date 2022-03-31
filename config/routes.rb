3e915106446542d47fda9d34b27d96a37b2cac73
Ruben Pasillas <hobbiathan@gmail.com>
2022-03-31 10:26:46 -0600

    specify controller in nested route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+
+      resources :items, only: [:index]
+
       resources :merchants, only: [:index, :show] do 
-        resources :items, only: [:index] do
-          
+        resources :items, controller: 'merchant_items', only: [:index] do
+
         end 
       end
+
+
     end 
   end 
 end
