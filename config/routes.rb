dc6347c6b9d912699ecbcec12d5f1911bb2ad03e
Ruben Pasillas <hobbiathan@gmail.com>
2022-03-31 14:27:16 -0600

    create item/:item_id/merchant route

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,17 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
 
-      resources :items
+      resources :items do
+        resources :merchant, controller: 'item_merchants', only: [:index] do
+
+        end
+      end 
 
       resources :merchants, only: [:index, :show] do 
         resources :items, controller: 'merchant_items', only: [:index] do
 
         end 
       end
-
 
     end 
   end 
