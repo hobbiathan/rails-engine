8021217b1ee65a6c480a860600cb37d2f3b2f776
Ruben Pasillas <hobbiathan@gmail.com>
2022-03-31 10:36:07 -0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
 
-      resources :items, only: [:index]
+      resources :items, only: [:index, :show]
 
       resources :merchants, only: [:index, :show] do 
         resources :items, controller: 'merchant_items', only: [:index] do
